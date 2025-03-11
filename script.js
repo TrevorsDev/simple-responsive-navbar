02dc1e3ba8fa0b4a18ef14fb970e90451a369d30
Trevor Johnson <trevor9j@protonmail.com>
2025-03-10 22:51:32 -0700

    add proper positioning, hover effects, and click events for dropdown menu

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const hamMenu = document.querySelector('.ham-menu');
-
 const navLinks = document.querySelector('.nav-links');
 
-hamMenu.addEventListener('click', () => {
+hamMenu.addEventListener('click', (event) => {
+  event.stopPropagation(); // Prevents the click from bubbling up to document
   hamMenu.classList.toggle('active');
   navLinks.classList.toggle('active');
-})
+});
+
+// Close the menu when clicking outside
+document.addEventListener('click', (event) => {
+  if (!navLinks.contains(event.target) && !hamMenu.contains(event.target)) {
+    hamMenu.classList.remove('active');
+    navLinks.classList.remove('active');
+  }
+});
